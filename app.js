7a356fea25c362f8681b06a173e57b67fd131ca1
Shylashree V <shylashree74@gmail.com>
2022-05-19 17:45:33 +0530

    commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const balance = document.getElementById("balance");
+const money_plus = document.getElementById('money-plus');
+const money_minus = document.getElementById('money-minus');
+const list = document.getElementById("list");
+const form = document.getElementById('form');
+const text = document.getElementById('text');
+const amount = document.getElementById('amount');
+
+const dummyTransactions = [
+    {id: 1, text: "Flower", amount: -20},
+    {id: 2, text: "Salary", amount: 300},
+    {id: 3, text: "Book", amount: -10},
+    {id: 4, text: "Camera", amount: 150},
+];
+
+let transactions = dummyTransactions;
+
+function addTransasctionDOM(transaction){
+    const sign = transaction[0].amount < 0 ? "-" : "+";
+    const item = document.createElement("li");
+
+    item.classList.add(
+        transaction[0].amount < 0 ? "minus" : "plus"
+    )
+
+    item.innerHTML = `
+    ${transaction[0].text}<span>${sign}${Math.abs(transaction[0].amount)}</span>
+    <button class="delete-btn" onclick="">x</button>
+    `;
+
+    list.appendChild(item);
+}
+
+addTransasctionDOM(Transactions);
